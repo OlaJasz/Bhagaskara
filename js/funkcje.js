969e2b560ababae0ee4cac3861c3db63ffedaf18
OlaJasz <a.jaszewska@gmail.com>
2015-08-30 17:49:16 +0200

    web

--- js/funkcje.js
+++ js/funkcje.js
@@ -1,34 +1,54 @@
 $(document).ready(function(){
 
-    $(".imagNav a").on('click', function(){
-        var href = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(href).offset().top
-        }, 'slow');
-        return false; //zeby sie nie przeladowala strona po klinknieciu na li
-    });
+    function scrollViewport() {
+        $(".imagNav a, .navList a").on('click', function(){
+            var href = $(this).attr('href');
+            $('html, body').animate({
+                scrollTop: $(href).offset().top
+            }, 'slow');
+            return false; //zeby sie nie przeladowala strona po klinknieciu na li
+        });
+    }
 
-    $(".navList a").on('click', function(){
-        var href = $(this).attr('href');
-        $('html, body').animate({
-            scrollTop: $(href).offset().top
-        }, 'slow');
-        return false; //zeby sie nie przeladowala strona po klinknieciu na li
-    });
-
-    $('.project').find('.img').mouseover(function(){
-        $(this).prev().show();
-    });
-    $('.project').find('.img').mouseout(function(){
-        $('.hover').hide();
-    });
-
-    $('.hover').find('img').on('click', function () {
-       console.log('klik');
-    });
-
-    //$('.hover').find('img').on('click', function(){
-    //    console.log('klik');
-    //    //$(this).addClass('bigPicture');
-    //});
+    function showHoverOnGalleryImage() {
+        $('.project').find('.img').mouseover(function(){
+            $(this).prev('.hover').show();
+        });
+        $('.hover').mouseout(function(){
+            $(this).hide();
+        });
+    }
+    scrollViewport();
+    showHoverOnGalleryImage();
 });
+
+
+    //function validateForm (){
+    //    var x = document.forms["message"]["imie"].value;
+    //    if (x == null || x ==""){
+    //        alert("Name must be filled out");
+    //        return false;
+    //    }
+    //}
+
+
+
+
+
+
+    //var greypercentege = $('.grey').width();
+    //var PinkpercentegeArray = [];
+    //
+    //for (var k=0; k < $('.boxes').children().length ; k++){
+    //    var percentageText = parseInt($(".percentage"+(k+1)).text(),10);
+    //    PinkpercentegeArray.push( (percentageText + "%"));
+    //}
+    //console.log(PinkpercentegeArray);
+    //
+    //for (var i=0; i < $('.boxes').children().length; i++) {
+    //    $('.boxes').find('.pinkFirst').eq(i).animate({
+    //        width: PinkpercentegeArray[i]
+    //    });
+    //}
+
+
